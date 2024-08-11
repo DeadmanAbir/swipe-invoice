3ba0823fbf89f163f75a5e8d4775d564f954ee91
Your Name <abirdutta90990@gmail.com>
2024-08-11 14:49:24 +0530

    first commit

---
+++ src/app/api/extractInvoice/core.ts
@@ -0,0 +1,97 @@
+import { UnstructuredClient } from "unstructured-client";
+import { PartitionResponse } from "unstructured-client/sdk/models/operations";
+import { Strategy } from "unstructured-client/sdk/models/shared/index.js";
+import { ChatOpenAI } from "@/utils/chatOpenAi";
+
+export const validateFile = async (
+  file: File
+): Promise<{ valid: boolean; message?: string }> => {
+  if (!file) {
+    return { valid: false, message: "No file provided." };
+  }
+
+  const allowedTypes = ["application/pdf", "image/jpeg", "image/png"];
+  if (!allowedTypes.includes(file.type)) {
+    return {
+      valid: false,
+      message: "Invalid file type. Only PDF, JPEG, and PNG files are allowed.",
+    };
+  }
+
+  const maxSizeInBytes = 5 * 1024 * 1024; // 5MB
+  if (file.size > maxSizeInBytes) {
+    return { valid: false, message: "File size exceeds the 5MB limit." };
+  }
+
+  return { valid: true };
+};
+
+export const extractInvoice = async (
+  file: File
+): Promise<string | undefined> => {
+  try {
+    const apiKey = process.env.UNSTRUCTURED_API_KEY;
+    if (!apiKey) throw new Error("UNSTRUCTURED_API_KEY is not set");
+
+    const unstructuredClient = new UnstructuredClient({
+      security: {
+        apiKeyAuth: apiKey,
+      },
+    });
+
+    const arrayBuffer = await file.arrayBuffer();
+    const uint8Array = new Uint8Array(arrayBuffer);
+
+    const res: PartitionResponse = await unstructuredClient.general.partition({
+      partitionParameters: {
+        files: {
+          content: uint8Array,
+          fileName: file.name,
+        },
+        strategy: Strategy.Auto,
+      },
+    });
+
+    if (res.statusCode === 200) {
+      if (!res.elements) {
+        return "No text found in the file.";
+      }
+
+      return res.elements.map((el) => el.text).join("\n");
+    } else {
+      throw new Error(`Unexpected status code: ${res.statusCode}`);
+    }
+  } catch (e: any) {
+    // Use a proper logging mechanism here instead of console.log
+    console.error("Error extracting invoice:", e.message);
+    console.error("Status Code:", e.statusCode);
+    console.error("Response Body:", e.body);
+    return undefined;
+  }
+};
+
+export const extractData = async (context: string) => {
+  try {
+    const apiKey = process.env.OPENAI_API_KEY;
+    if (!apiKey) throw new Error("OPENAI_API_KEY is not set");
+
+    const chatOpenAI = new ChatOpenAI({
+      apiKey: apiKey,
+      model: "gpt-4",
+    });
+
+    const data = await chatOpenAI.chat({
+      prompt:
+        "We are providing you with the context of an invoice data, your job is to extract the customer details, products and total amount from the invoice data.",
+      context: context,
+      systemInstruction:
+        "You're a data extractor, your job is to extract data from invoices data provided.",
+      outputFormat: `{"customer_name": "", "customer_address": "","customer_number": "","customer_mail": "", "products": "", "total_amount": ""}`,
+    });
+
+    return data;
+  } catch (e: any) {
+    console.error("Error extracting data:", e.message);
+    return undefined;
+  }
+};
---
+++ src/app/api/extractInvoice/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import { extractData, extractInvoice, validateFile } from "./core";
+
+export async function POST(request: NextRequest) {
+  const data = await request.formData();
+  const file: File | null = data.get("file") as File;
+  const validation = await validateFile(file);
+
+  if (!validation.valid) {
+    return NextResponse.json({ error: validation.message }, { status: 400 });
+  }
+
+  const extractedText = await extractInvoice(file);
+
+  if (!extractedText || extractedText === "No text found in the file.") {
+    return NextResponse.json(
+      { error: "No text found in the file." },
+      { status: 406 }
+    );
+  }
+
+  const extractedResponse = await extractData(extractedText);
+
+  if (!extractedResponse) {
+    return NextResponse.json(
+      { error: "Failed to extract data." },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json({ data: extractedResponse }, { status: 200 });
+}
