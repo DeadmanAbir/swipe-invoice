fcc375a55e5b01761da188691fbc744c4f5952f1
faisal004 <faisalhusain1320@gmail.com>
2024-08-11 16:08:29 +0530

    added : design change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useState } from "react";
+'use client'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { useEffect, useState } from 'react'
 import {
   Table,
   TableBody,
@@ -9,42 +9,57 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table";
+} from '@/components/ui/table'
+import { Eraser, Trash, Trash2 } from 'lucide-react'
 
 export default function Home() {
-  const [files, setFiles] = useState<FileList | null>(null);
-  const [fileURL, setFileURL] = useState<string | null>(null);
-  const [invoiceData, setInvoiceData] = useState(null);
+  const [files, setFiles] = useState<FileList | null>(null)
+  const [fileURL, setFileURL] = useState<string | null>(null)
+  const [invoiceData, setInvoiceData] = useState(null)
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const fileList = event.target.files;
-    setFiles(fileList);
+    const fileList = event.target.files
+    setFiles(fileList)
 
     if (fileList && fileList[0]) {
-      const url = URL.createObjectURL(fileList[0]);
-      setFileURL(url);
+      const url = URL.createObjectURL(fileList[0])
+      setFileURL(url)
     }
-  };
-
+  }
+  useEffect(() => {
+    const storedInvoiceData = localStorage.getItem('invoiceData')
+    if (storedInvoiceData) {
+      setInvoiceData(JSON.parse(storedInvoiceData))
+    }
+  }, [])
+  useEffect(() => {
+    if (invoiceData) {
+      localStorage.setItem('invoiceData', JSON.stringify(invoiceData))
+    }
+  }, [invoiceData])
   const handleSubmit = () => {
     if (files) {
-      const formData = new FormData();
-      formData.set("file", files[0]);
+      const formData = new FormData()
+      formData.set('file', files[0])
 
-      fetch("/api/extractInvoice", {
-        method: "POST",
+      fetch('/api/extractInvoice', {
+        method: 'POST',
         body: formData,
       })
         .then((response) => response.json())
         .then((data) => {
-          const parsedData = JSON.parse(data.data.output);
-          setInvoiceData(parsedData);
+          const parsedData = JSON.parse(data.data.output)
+          setInvoiceData(parsedData)
         })
         .catch((error) => {
-          console.error("Error:", error);
-        });
+          console.error('Error:', error)
+        })
     }
-  };
+  }
+  const handleClearStorage = () => {
+    localStorage.removeItem('invoiceData')
+    setInvoiceData(null)
+  }
 
   return (
     <div className="min-h-screen grid md:grid-cols-2 grid-cols-1 gap-2 justify-center items-center pt-20 max-w-[1440px] mx-auto p-3">
@@ -70,10 +85,8 @@ export default function Home() {
         )}
       </div>
       <div className="flex flex-col items-center gap-2 justify-center w-full">
-        <div className="text-2xl font-semibold">
-          Invoice Extractor
-        </div>
-        
+        <div className="text-2xl font-semibold">Invoice Extractor</div>
+
         {invoiceData && (
           <Table>
             <TableBody className="border-2">
@@ -104,7 +117,13 @@ export default function Home() {
             </TableBody>
           </Table>
         )}
+        <div className='flex items-center justify-end w-full'>
+        {invoiceData && (
+          <Button onClick={handleClearStorage}><Trash2 className='h-5 w-5'/></Button>
+        )}
+        </div>
+       
       </div>
     </div>
-  );
+  )
 }
