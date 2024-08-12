9b167036a1a32bfd421692c56ff0904eac75b997
Your Name <abirdutta90990@gmail.com>
2024-08-12 12:17:01 +0530

    Extending duration for api routes

--- src/app/api/extractInvoice/route.ts
+++ src/app/api/extractInvoice/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { extractData, extractInvoice, validateFile } from "./core";
 
+export const maxDuration = 300;
+
 export async function POST(request: NextRequest) {
   const data = await request.formData();
   const file: File | null = data.get("file") as File;
